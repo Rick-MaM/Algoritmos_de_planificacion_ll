89e129fefce34bf12357759dadfda063a5092c39
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-14 18:49:15 -0600

    Se crea la ventana de agregar

--- Practica_4.py
+++ Practica_4.py
@@ -8,7 +8,8 @@ class Window:
         self.window.title("Algoritmos de planificacion")
         self.window.geometry("450x350")
 
-        self.Open_File()
+
+        self.band_add = False
 
         bar_menu = Menu(self.window)
 
@@ -43,18 +44,57 @@ class Window:
         Label(self.window, text="-----> Tiempo <-----",fg="BLUE").place(x=20, y=220)
     
     def Add_Process(self):
-        pass
+        window_add = Tk()
+        window_add.title("Agregar")
+        window_add.geometry("300x250")
+        Label(window_add, text="Nombre de proceso").place(x=10,y=10)
+        self.txtProcess = Entry(window_add, width=20)
+        self.txtProcess.place(x=10,y=40)
+        Label(window_add, text="Prioridad").place(x=10,y=70)
+        self.txtPriority = Entry(window_add, width=10)
+        self.txtPriority.place(x=10, y=100)
+        Label(window_add, text="Tiempo").place(x=10,y=130)
+        self.txtTime = Entry(window_add, width=10)
+        self.txtTime.place(x=10, y=160)
+        Label(window_add, text="Posicion").place(x=10, y=190)
 
-    def Imprimir(self):
-        pass
+        variable = StringVar()
+        self.option = ttk.Combobox(window_add, width=17,textvariable=variable)
+        self.option["values"] = ("Al final","Al principio")
+        self.option.place(x=10,y=220)
+        self.option.current(0)
+
+        btnAdd_process = Button(window_add, text="Agregar",command=self.new_process)
+        btnAdd_process.place(x=200,y=20)
+
+        window_add.mainloop
+
+    def new_process(self):
+        self.new = self.txtProcess.get() + "," + self.txtPriority.get() + "," + self.txtTime.get()
+        self.band_add = True
+
+    def current_list(self,list_process):
+        if self.option.get() == "Al final":
+            list_process.append(self.new)
+            return list_process
+        elif self.option.get() == "Al principio":
+            new_list_process = []
+            new_list_process.append(self.new)
+            new_list_process = new_list_process + list_process
+            return new_list_process
              
     def Open_File(self):
         with open("procesos.txt","r") as file:
-            self.list_process = file.readlines()
+            list_process = file.readlines()
+        return list_process
     
     def FIFO(self):
-        List_Process_FIFO = self.list_process
+        List_Process_FIFO = self.Open_File()
         while len(List_Process_FIFO) != 0:
+            if self.band_add:
+                List_Process_FIFO = self.current_list(List_Process_FIFO)
+                self.band_add = False
+                print("Se agrego")
             process = List_Process_FIFO[0].split(",")
             load = ""
             lblprocess = Label(self.frame, text=process[0])
