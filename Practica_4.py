0b20e427151e5550d020656198dd30be0bb469c8
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-16 01:23:14 -0600

    Actualizacion del los planificadores

--- Practica_4.py
+++ Practica_4.py
@@ -9,15 +9,20 @@ class Window:
         self.window.geometry("600x260")
 
         self.band_add = False
+        self.stop_process = False
         self.list_new_processes = []
+        self.process_support = []
+
+        btnStop = Button(self.window, text="Finalizar", command=self.stop)
+        btnStop.place(x=500,y=0)
 
         bar_menu = Menu(self.window)
 
         menu_process = Menu(bar_menu)
-        menu_process.add_command(label="Round Robin",command=self.Round_Robin)
-        menu_process.add_command(label="SJF",command=self.SJF)
-        menu_process.add_command(label="FIFO",command=self.FIFO)
-        menu_process.add_command(label="Prioridad",command=self.priority)
+        menu_process.add_command(label="Round Robin",command=self.menu_Round_Robin)
+        menu_process.add_command(label="SJF",command=self.menu_SJF)
+        menu_process.add_command(label="FIFO",command=self.menu_FIFO)
+        menu_process.add_command(label="Prioridad",command=self.menu_priority)
         bar_menu.add_cascade(label="Algoritmos", menu=menu_process)
 
         menu_Add = Menu(bar_menu)
@@ -65,6 +70,53 @@ class Window:
         btnAdd_process.place(x=200,y=20)
 
         window_add.mainloop
+    
+    def menu_Round_Robin(self):
+        lblProcess = Label(self.frame, text="---> Round_Robin <---",fg="BLUE")
+        lblProcess.place(x=20,y=20)
+        while not self.stop_process:
+            List_Round_Robin = self.Open_File()
+            List_Round_Robin = self.current_list(List_Round_Robin)
+            self.Round_Robin(List_Round_Robin)
+            self.change_list()
+        lblProcess.destroy()
+        self.stop_process = False
+
+    def menu_SJF(self):
+        lblProcess = Label(self.frame, text="---> SJF <---", fg="BLUE")
+        lblProcess.place(x=20, y=20)
+        while not self.stop_process:
+            List_SJF = self.Open_File()
+            List_SJF = self.current_list(List_SJF)
+            self.SJF(List_SJF)
+            self.change_list()
+        self.stop_process = False
+        lblProcess.destroy()
+    
+    def menu_FIFO(self):
+        lblProcess = Label(self.frame, text="---> FIFO <---", fg="BLUE")
+        lblProcess.place(x=20, y=20)
+        while not self.stop_process:
+            List_FIFO = self.Open_File()
+            List_FIFO = self.current_list(List_FIFO)
+            self.FIFO(List_FIFO)
+            self.change_list()
+        self.stop_process = False
+        lblProcess.destroy()
+    
+    def menu_priority(self):
+        lblProcess = Label(self.frame, text="---> Prioridad <---", fg="BLUE")
+        lblProcess.place(x=20, y=20)
+        while not self.stop_process:
+            List_priority = self.Open_File()
+            List_priority = self.current_list(List_priority)
+            self.priority(List_priority)
+            self.change_list()
+        self.stop_process = False
+        lblProcess.destroy()
+
+    def stop(self):
+        self.stop_process = True
 
     def new_process(self):
         new = self.txtProcess.get() + "," + self.txtPriority.get() + "," + self.txtTime.get() + "," + self.option.get()
@@ -76,6 +128,7 @@ class Window:
         self.band_add = True
 
     def current_list(self,list_process):
+        self.process_support = self.process_support + self.list_new_processes
         while len(self.list_new_processes) != 0:
             new_list_process = []
             new_process = self.list_new_processes[0].split(",")
@@ -90,25 +143,26 @@ class Window:
 
             self.list_new_processes.pop(0)
         return list_process
+    
+    def change_list(self):
+        self.list_new_processes = self.process_support
              
     def Open_File(self):
         with open("procesos.txt","r") as file:
             list_process = file.readlines()
         return list_process
     
-    def Round_Robin(self):
-        List_Process_Round_Robin = self.Open_File()
-        quantum = 3
+    def Round_Robin(self, List_Process_Round_Robin):
         while len(List_Process_Round_Robin) != 0:
             if self.band_add:
-                List_Process_priority = self.current_list(List_Process_priority)
+                List_Process_Round_Robin = self.current_list(List_Process_Round_Robin)
                 self.band_add = False
             process = List_Process_Round_Robin[0].split(",")
             time_process = int(process[2])
             self.destroy_or_insert_label(process[0], process[1], process[2], True)
             self.window.update()
 
-            for count_time in range(quantum):
+            for count_time in range(3):
                 if time_process == 0:
                     break
                 else:
@@ -126,9 +180,10 @@ class Window:
             self.destroy_or_insert_label(process[0], process[1], process[2], False)
             self.process_counting(count_time, False)
             List_Process_Round_Robin.pop(0)
+            if self.stop_process:
+                break
 
-    def SJF(self):
-        List_Process_SJF = self.Open_File()
+    def SJF(self, List_Process_SJF):
         times = self.sort_process(List_Process_SJF,2)
         number_process = len(List_Process_SJF)
         while len(List_Process_SJF) != 0:
@@ -153,9 +208,11 @@ class Window:
             List_Process_SJF.pop(count_process)
             number_process = len(List_Process_SJF)
             times.pop(0)
+            if self.stop_process:
+                break
     
-    def FIFO(self):
-        List_Process_FIFO = self.Open_File()
+    def FIFO(self, List_Process_FIFO):
+        
         while len(List_Process_FIFO) != 0:
             if self.band_add:
                 List_Process_FIFO = self.current_list(List_Process_FIFO)
@@ -172,9 +229,11 @@ class Window:
             self.destroy_or_insert_label(process[0], process[1], process[2], False)
             self.process_counting(count, False)
             List_Process_FIFO.pop(0)
+            if self.stop_process:
+                break
 
-    def priority(self):
-        List_Process_priority = self.Open_File()
+    def priority(self, List_Process_priority):
+        
         list_priority = self.sort_process(List_Process_priority, 1)
         number_process = len(List_Process_priority)
 
@@ -199,6 +258,8 @@ class Window:
             List_Process_priority.pop(count_process)
             number_process = len(List_Process_priority)
             list_priority.pop(0)
+            if self.stop_process:
+                break
     
     def sort_process(self, list_SJF, time_or_priority):
         sort_process = []
@@ -211,13 +272,13 @@ class Window:
     def destroy_or_insert_label(self,name,priority,time,insert):
         if insert:
             self.lblprocess = Label(self.frame, text=f"Proceso: {name}")
-            self.lblprocess.place(x=20, y=20)
+            self.lblprocess.place(x=20, y=50)
 
             self.lblpriority = Label(self.frame, text=f"Prioridad : {priority}")
-            self.lblpriority.place(x=20, y=50)
+            self.lblpriority.place(x=20, y=80)
 
             self.lbltime = Label(self.frame, text=f"Tiempo: {time}")
-            self.lbltime.place(x=20, y=80)
+            self.lbltime.place(x=20, y=110)
             
         else:
             self.lblprocess.destroy()
@@ -228,12 +289,11 @@ class Window:
     def process_counting(self,count_time,insert):
         if insert:
             self.lblcount_time = Label(self.frame, text=f"Tiempo de ejecucion: {count_time + 1}")
-            self.lblcount_time.place(x=20,y=110)
+            self.lblcount_time.place(x=20,y=140)
         else:
             self.lblcount_time.destroy()
         self.window.update()
         
-
 if __name__ == "__main__":
     ventana = Tk()
     apliaction = Window(ventana)
